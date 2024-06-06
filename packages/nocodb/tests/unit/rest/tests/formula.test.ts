1b260975efe586f3d86d2063219a2c69d08f76ba
mertmit <mertmit99@gmail.com>
2024-06-06 19:03:18 +0300

    fix: unit test

--- packages/nocodb/tests/unit/rest/tests/formula.test.ts
+++ packages/nocodb/tests/unit/rest/tests/formula.test.ts
@@ -26,7 +26,7 @@ function formulaRegExpBased() {
     base = await createProject(context);
 
     ctx = {
-      workspace_id: base.workspace_id,
+      workspace_id: base.fk_workspace_id,
       base_id: base.id,
     };
 
